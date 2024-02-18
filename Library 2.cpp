bd873ba90dcfb3132b377b5ab93f5d919a5daf95
beykiin <132366582+beykiin@users.noreply.github.com>
2024-02-18 14:08:11 +0300

    Add files via upload

---
+++ Library 2.cpp
@@ -0,0 +1,54 @@
+﻿// Library 2.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+#include <string>
+
+//using namespace std;
+//
+//int main()
+//{
+//    string Open = "Hello String";
+//    cout << Open.empty();
+//}
+
+//using namespace std;
+//
+//int main()
+//{
+//    string Open = "";
+//    cout << Open.empty();
+//}
+
+//using namespace std;
+//
+//int main()
+//{
+//    string Open = "Hello String";
+//    string Close = "This Page is Example!";
+//    Open.insert(6, "Start ");
+//    cout << Open << endl;
+//    cout << Close << endl;
+//}
+
+//using namespace std;
+//
+//int main()
+//{
+//    string Open = "Hello String";
+//    string Close = "This Page is Example!";
+//    Open.append(" Start ");
+//    cout << Open << endl;
+//    cout << Close << endl;
+//}
+
+// Programı çalıştır: Ctrl + F5 veya Hata Ayıkla > Hata Ayıklamadan Başlat menüsü
+// Programda hata ayıkla: F5 veya Hata Ayıkla > Hata Ayıklamayı Başlat menüsü
+
+// Kullanmaya Başlama İpuçları: 
+//   1. Dosyaları eklemek/yönetmek için Çözüm Gezgini penceresini kullanın
+//   2. Kaynak denetimine bağlanmak için Takım Gezgini penceresini kullanın
+//   3. Derleme çıktısını ve diğer iletileri görmek için Çıktı penceresini kullanın
+//   4. Hataları görüntülemek için Hata Listesi penceresini kullanın
+//   5. Yeni kod dosyaları oluşturmak için Projeye Git > Yeni Öğe ekle veya varolan kod dosyalarını projeye eklemek için Proje > Var Olan Öğeyi Ekle adımlarını izleyin
+//   6. Bu projeyi daha sonra yeniden açmak için Dosya > Aç > Proje'ye gidip .sln uzantılı dosyayı seçin
